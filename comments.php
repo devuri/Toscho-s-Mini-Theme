0ec47b06ed1ccb6bc79776471d244c7eaad0269c
toscho <info@toscho.de>
2012-11-23 23:00:55 +0100

    Shortened code
    Signed-off-by: toscho <info@toscho.de>

--- comments.php
+++ comments.php
@@ -1,15 +1,13 @@
 <?php
-if ( ! defined('ABSPATH') ) { die ('Nö.'); }
-
 if ( $comments )
 {
-?><h2 id="comments"><?php
-	comments_number( 'Kein Kommentar', 'Ein Kommentar', '% Kommentare' );
-?></h2>
-<ol class="commentlist">
-<?php
+	?><h2 id="comments"><?php
+		comments_number();
+	?></h2>
+	<ol class="commentlist">
+	<?php
+	wp_list_comments();
+	?></ol>
+	<?php
 }
-wp_list_comments( array ( 'type'  => 'comment', 'style' => 'ul' ) );
-?></ol>
-<?php
-comment_form();
+comments_open( get_the_ID() ) and comment_form();
--- index.php
+++ index.php
@@ -6,50 +6,43 @@
 <!doctype html>
 <html <?php language_attributes(); ?> >
 <link rel=stylesheet href="<?php echo get_stylesheet_uri(); ?>">
-<title><?php wp_title( ' ' ); ?></title>
+<title><?php
+$title = wp_title( ' ', FALSE );
+print empty ( $title ) ? get_bloginfo( 'name' ) : $title;
+?></title>
 <?php wp_head(); ?>
 
 <body <?php body_class(); ?>>
 <ul><?php wp_list_pages( array ( 'title_li' => FALSE ) ); ?></ul>
 
-<h1><?php the_title(); ?></h1>
-
 <?php
 if ( have_posts() )
 {
-	! is_singular() and print '<ul>';
-
 	while ( have_posts() )
 	{
 		the_post();
 
-		print is_singular() ? '<div' : '<li';
-		print ' class="' . join( ' ', get_post_class() ) . '">' . "\n\t";
+		print '<div class="' . join( ' ', get_post_class() ) . '">' . "\n\t";
 
-		if ( is_singular() )
-		{
-			the_content();
-			wp_link_pages();
-		}
-		else
-		{
-			print '<h2><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
-			the_excerpt();
-		}
+		print '<h2><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
+		the_content();
+		wp_link_pages();
 
-		print is_singular() ? "</div>\n" : "</li>\n";
+		print '</div>';
 	}
 
 	comments_template();
-
-	! is_singular() and print '</ul>';
 }
 else
 {
 	?>
-	Sorry, gibt’s hier nicht.
+	<p>Not found</p>
 	<?php
 }
 
-is_singular() and get_option( 'thread_comments' ) and wp_print_scripts( 'comment-reply' );
+is_singular()
+	and get_option( 'thread_comments' )
+	and comments_open( get_the_ID() )
+	and wp_enqueue_script( 'comment-reply' );
+
 wp_footer();
